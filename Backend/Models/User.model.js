ed58422b08e7df57542a7d98f76ce6069f54f1a5
Pawan Kumar <101585236+rajpawanku@users.noreply.github.com>
2022-12-14 10:47:50 +0530

    login update

--- Backend/Models/User.model.js
+++ Backend/Models/User.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
      number : Number,
-    password : String,
+     password : String,
    
 })
 
--- Backend/Routes/login.route.js
+++ Backend/Routes/login.route.js
@@ -1,38 +1,56 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
-const app = express();
-app.use(express.json())
+const bcrypt = require("bcrypt")
 
 
-const {LoginModel} = require("../Models/User.model")
+const {UserModel} = require("../Models/User.model")
+const loginRouter = express.Router();
 
-app.post("/signup", async (req, res) => {
-    const payload = req.body;
-    try{
-        const user = new LoginModel(payload)
-        await user.save()
-        res.send("Sign up successfull")
-    }
-   catch(err){
-        console.log(err)
-        res.send("Something went wrong, pls try again later")
-   }
+loginRouter.post("/signup", async (req, res) => {
+  console.log(req.body)
+  const {number, password} = req.body;
+  const userPresent = await UserModel.findOne({number})
+  if(userPresent?.number){
+      res.send("Try loggin in, already exist")
+  }
+  else{
+      try{
+          bcrypt.hash(password, 4, async function(err, hash) {
+              const user = new UserModel({number,password:hash})
+              await user.save()
+              res.send("Sign up successfull")
+          });
+         
+      }
+     catch(err){
+          console.log(err)
+          res.send("Something went wrong, pls try again later")
+     }
+  }
+  
 })
 
-app.post("/login", async (req, res) => {
-    const {email, password} = req.body;
-    try{
-      const user = await LoginModel.find({email, password})
-      if(user.length > 0){
-        const token = jwt.sign({"course":"nxm" }, 'hush');
-        res.send({"msg":"Login successfull","token" : token})
-      } 
-      else{
-        res.send("Login failed")
-      }
+loginRouter.post("/login", async (req, res) => {
+  const {number, password} = req.body;
+  try{
+      const user = await UserModel.find({number})
+       
+    if(user.length > 0){
+      const hashed_password = user[0].password;
+      bcrypt.compare(password, hashed_password, function(err, result) {
+          if(result){
+              const token = jwt.sign({"userID":user[0]._id}, 'hush');
+              res.send({"msg":"Login successfull","token" : token})
+          }
+          else{
+              res.send("Login failed")
+          }
+    })} 
+    else{
+      res.send("Login failed")
     }
-    catch{
-        res.send("Something went wrong, please try again later")
-    }
-    res.send("work in progress")
+  }
+  catch{
+      res.send("Something went wrong, please try again later")
+  }
 })
