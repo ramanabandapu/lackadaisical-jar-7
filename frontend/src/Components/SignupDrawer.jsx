83cb864606490d23c02f24db34c1fd7b02150573
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-12-14 21:51:31 +0530

    Login and Signup UI

---
+++ frontend/src/Components/SignupDrawer.jsx
@@ -0,0 +1,43 @@
+import Signup from "./Signup";
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+
+export const SignupDrawer = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+  return (
+    <Box>
+      <Text style={{ cursor: "pointer" }} ref={btnRef} onClick={onOpen} color= "blue">
+        SignUp
+      </Text>
+      {/* Drawer Start */}
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">
+            Create a new account
+          </DrawerHeader>
+          <DrawerBody>
+            <Stack spacing="24px">
+              <Box>
+                <Signup onClose ={onClose}/>
+              </Box>
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+};
