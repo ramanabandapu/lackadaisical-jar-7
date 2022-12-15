1727d97426a5b05e155c5ab31f1f24f0b67284a1
Prabhash Kumar <prabhash.kumar96@gmail.com>
2022-12-16 00:46:37 +0530

    resoleve merge conflict

--- frontend/src/Routes/MainRoutes.jsx
+++ frontend/src/Routes/MainRoutes.jsx
@@ -1,31 +1,26 @@
-
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../Components/HomePage";
+import SingleProductPage from "./SingleProductPage";
 
 const MainRoutes = () => {
   return (
     <Box>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/page" element={<SingleProductPage />} />
       </Routes>
     </Box>
   );
 };
 
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import SingleProductPage from './SingleProductPage';
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import SingleProductPage from "./SingleProductPage";
 
-const MainRoutes = () => {
-  
-    return (
-      <Routes>
-        <Route path="/page" element={<SingleProductPage />} />
-      </Routes>
-    );
-}
-
+// const MainRoutes = () => {
+//   return <Routes></Routes>;
+// };
 
 export default MainRoutes;
