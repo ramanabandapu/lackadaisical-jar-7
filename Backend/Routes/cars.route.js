b856041e71764484fd363eb168ed342f9dba1514
Pawan Kumar <101585236+rajpawanku@users.noreply.github.com>
2022-12-15 20:00:48 +0530

    add

--- Backend/Routes/cars.route.js
+++ Backend/Routes/cars.route.js
@@ -14,5 +14,16 @@ carsRouter.get("/cars",async(req,res) =>{
     
    
 })
+const searchProduct = async (req, res) => {
+    let { q } = req.query;
+    try {
+      let cars = await CarModel.find({ $search: { title: q } });
+      console.log(q);
+      res.send(cars);
+    } catch (err) {
+      res.send({ msg: "Something went wrong", error: err });
+    }
+  };
+
 
 module.exports = {carsRouter}
