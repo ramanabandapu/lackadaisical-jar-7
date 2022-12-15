8aadf95845136bd5658c2a4c8c6126943d9a91ae
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-12-15 14:30:11 +0530

    login functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,18 @@ import reportWebVitals from "./reportWebVitals";
 
 import {ChakraProvider} from "@chakra-ui/react";
 import {BrowserRouter }from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- 
+ <Provider store = {store}>
   <ChakraProvider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </ChakraProvider>
-
+  </Provider>
        
      
 );
