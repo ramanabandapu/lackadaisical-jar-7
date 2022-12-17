409b75940e2e31a4c91481644cf7694ea9d62351
Pawan Kumar <101585236+rajpawanku@users.noreply.github.com>
2022-12-17 23:40:46 +0530

    app.js update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,19 @@ import Footer from "./Components/Footer";
 import Motor from "./Components/ankesh_work/Motor";import Navbar from "./Components/Navbar";
 
 import MainRoutes from "./Routes/MainRoutes";
-import HomePage from "./Components/HomePage";
 
 
 function App() {
   return (
     <div className="App">
-{/* <LoginDrawer/> */}
-{/* <SignupDrawer/> */}
-<HomePage />
+<LoginDrawer/>
+<SignupDrawer/>
+{/* <HomeSection/> */}
+{/* <Login/> */}
 
 
-
-      {/* <Navbar/> */}
-      {/* <MainRoutes/> */}
+      <Navbar/>
+      <MainRoutes/>
 <Footer/>
 
     </div>
